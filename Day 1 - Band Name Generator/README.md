0cd5e8a9b25b0722c937cb5f8dbccec2ca565716
Bluu <maciejczak@dotbluu.eu>
2022-07-20 04:19:55 +0200

    Day one of 100 learning Python

---
+++ Day 1 - Band Name Generator/README.md
@@ -0,0 +1,8 @@
+# Day 1 - Band Name Generator
+## Concepts Practised
+- Printing to the Console in Python
+- String Manipulation and Code Intelligence
+- Debugging
+- The Python Input Function
+- Python Variables
+- Variable Naming
--- Day 1 - Band Name Generator/main.py
+++ Day 1 - Band Name Generator/main.py
@@ -1,4 +1,4 @@
 print("Welcome to the band name generator.")
 city = input("What's name of the city you grew up?\n")
-pet_name = input("Waht's your pet's name?\n")
+pet_name = input("What's your pet's name?\n")
 print("Your band name could be " + city + " " + pet_name)
